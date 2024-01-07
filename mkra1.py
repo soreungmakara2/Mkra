5744221e1a214144bb28c437dbcc4340170a3348
Makara <152614418+soreungmakara2@users.noreply.github.com>
2024-01-08 00:51:59 +0700

    Update mkra1.py

--- mkra1.py
+++ mkra1.py
@@ -19,9 +19,9 @@ url=''
 host=''
 headers_useragents=[]
 headers_referers=[]
-request_counter=0.5
-flag=0.0
-safe=0.5
+request_counter=1
+flag=1
+safe=1
 
 def inc_counter():
 	global request_counter
